25deb7f29db6702eb462d0d25075dd7c685b0964
touchesir <mrp10ss0n>
2016-03-07 21:07:20 -0800

    Added classifier script

---
+++ classifier.py
@@ -0,0 +1,38 @@
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+from csv import DictReader
+
+
+def collectComments(subs, file='comments.csv'):
+    data = []
+    targets = []
+    mappings = dict((sub, idx) for idx, sub in enumerate(subs))
+
+    with open(file) as csvdata:
+        reader = DictReader(csvdata)
+        for row in reader:
+            if mappings.has_key(row['subreddit']):
+                data.append(row['body'])
+                targets.append(mappings[row['subreddit']])
+
+    return data, targets
+
+
+if __name__ == '__main__':
+    subs = ['news', 'worldnews', 'nottheonion', 'circlejerk', 'atheism', 'gaming']
+    data, targets = collectComments(subs)
+
+    tests = ['Florida man', 'fake', 'fps']
+
+    text_clf = Pipeline([('vect', CountVectorizer()),
+                         ('tfidf', TfidfTransformer()),
+                         ('clf', MultinomialNB()),
+                         ])
+    text_clf = text_clf.fit(data, targets)
+
+    predicted = text_clf.predict(tests)
+
+    for doc, category in zip(tests, predicted):
+        print('%r => %s' % (doc, subs[category]))
